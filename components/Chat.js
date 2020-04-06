7f333c626551e772689592339b09bd8cd65d1dcb
cgobbet <cassiano.gobbet@gmail.com>
2020-04-06 19:45:08 +0200

    Update Chat.js
    Changes to allow background color settings

--- components/Chat.js
+++ components/Chat.js
@@ -1,23 +1,32 @@
-import { Button, Text, TextInput, View } from "react-native";
 import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default class Chat extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: "" };
-  }
-
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.state.params.name,
+      style: navigation.state.params.background,
+    };
+  };
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello {this.props.navigation.state.params.name}!</Text>
+      <View
+        style={[
+          styles.container,
+          { backgroundColor: this.props.navigation.state.params.color },
+        ]}
+      >
+        <Text style={{ color: "#FFFFFF" }}>Chat Screen</Text>
       </View>
     );
   }
 }
 
-// static navigationOptions = ({ navigation }) => {
-//   return {
-//     title: navigation.state.params.name,
-//   };
-// };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
