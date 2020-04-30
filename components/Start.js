618174f13a8f884f1c94ab3fc453bd713c232a56
cgobbet <cassiano.gobbet@gmail.com>
2020-04-30 11:39:53 +0200

    Full resshufle the Start buttons and styling
     due to dropping custom button provided by apsl-react-native-button, which led to conflicts (loader not appropriate)

--- components/Start.js
+++ components/Start.js
@@ -1,5 +1,8 @@
 import {
+  Alert,
+  Button,
   ImageBackground,
+  Platform,
   ScrollView,
   StyleSheet,
   Text,
@@ -9,10 +12,11 @@ import {
 } from "react-native";
 import React, { Component } from "react";
 
-import Button from "apsl-react-native-button";
+// import Button from "apsl-react-native-button"; // import custom buttons
 import KeyboardSpacer from "react-native-keyboard-spacer";
 
-// import custom buttons
+const firebase = require('firebase');
+require('firebase/firestore');
 
 export default class Start extends Component {
   constructor(props) {
@@ -29,160 +33,146 @@ export default class Start extends Component {
 			<ImageBackground
 				style={styles.backgroundImage}
 				source={require('../assets/background.png')}
-				// source={require("../assets/dark_back.jpg")}
 			>
 				<Text style={styles.title}>ChatTo</Text>
+
+				{/* Container for TextInput */}
 				<View style={styles.container}>
-					<TextInput
-						style={styles.textInput}
-						onChangeText={name => this.setState({ name })}
-						value={this.state.name}
-						placeholderTextColor={'black'}
-						placeholder='Your name...'
-						backgroundColor={'#F0F8FF'}
-						// opacity='0.5'
-					/>
+					<View style={styles.box}>
+						{/* Name to be displayed in header */}
+
+						<TextInput
+							style={[styles.textInput, styles.alignment]}
+							onChangeText={name => this.setState({ name })}
+							value={this.state.name}
+							placeholder='Your Name'
+						/>
+					</View>
+					<View style={styles.box}>
+						<Text style={styles.alignment}>Choose a background color</Text>
+
+						{/* Color options */}
+						<View style={styles.pickedColor}>
+							<TouchableOpacity
+								onPress={() => this.setState({ selectedColor: '#090C08' })}
+								style={[styles.button1, styles.colorOption]}
+							/>
+
+							<TouchableOpacity
+								onPress={() => this.setState({ selectedColor: '#474056' })}
+								style={[styles.button2, styles.colorOption]}
+							/>
+							<TouchableOpacity
+								onPress={() => this.setState({ selectedColor: '#8A95A5' })}
+								style={[styles.button3, styles.colorOption]}
+							/>
+							<TouchableOpacity
+								onPress={() => this.setState({ selectedColor: '#B9C6AE' })}
+								style={[styles.button4, styles.colorOption]}
+							/>
+						</View>
+					</View>
+					<View style={styles.box}>
+						<TouchableOpacity
+							accessible={true}
+							accessibilityLabel='More options'
+							accessibilityHint='Let’s you choose to send an image or your geolocation.'
+							accessibilityRole='button'
+							style={[styles.chatButton, styles.alignment]}
+							onPress={() => {
+								this.props.navigation.navigate('Chat', {
+									name: this.state.name,
+									selectedColor: this.state.selectedColor,
+								});
+							}}
+						>
+							<Text style={[styles.buttonText, styles.alignment]}>Start Chatting</Text>
+						</TouchableOpacity>
+					</View>
 				</View>
-				<View style={styles.box}></View>
-				<Text>Choose a background color:</Text>
-				<View style={styles.colorBackground}>
-					<TouchableOpacity
-						accessible={true}
-						accessibilityLabel='More options'
-						accessibilityHint='Allows you to choose to send an image or your geolocation.'
-						accessibilityRole='button'
-						onPress={() => this.setState({ color: '#090C08' })}
-						style={[styles.option1, styles.colorIcon]}
-					/>
-					<TouchableOpacity
-						accessible={true}
-						accessibilityLabel='More options'
-						accessibilityHint='Allows you to choose to send an image or your geolocation.'
-						accessibilityRole='button'
-						onPress={() => this.setState({ color: '#474056' })}
-						style={[styles.option2, styles.colorIcon]}
-					/>
-					<TouchableOpacity
-						accessible={true}
-						accessibilityLabel='More options'
-						accessibilityHint='Allows you to choose to send an image or your geolocation.'
-						accessibilityRole='button'
-						onPress={() => this.setState({ color: '#8A95A5' })}
-						style={[styles.option3, styles.colorIcon]}
-					/>
-					<TouchableOpacity
-						accessible={true}
-						accessibilityLabel='More options'
-						accessibilityHint='Allows you to choose to send an image or your geolocation.'
-						accessibilityRole='button'
-						onPress={() => this.setState({ color: '#B9C6AE' })}
-						style={[styles.option4, styles.colorIcon]}
-					/>
-				</View>
-				<View style={{ margin: 10 }}>
-					<Button
-						accessible={true}
-						accessibilityLabel='Start Chatting'
-						style={styles.button}
-						onPress={() =>
-							this.props.navigation.navigate('Chat', {
-								name: this.state.name,
-								color: this.state.color,
-							})
-						}
-					>
-						Start Chatting
-					</Button>
-					{Platform.OS === 'android' ? <KeyboardSpacer /> : null}
-				</View>
+				{Platform.OS === 'android' ? <KeyboardSpacer /> : null}
 			</ImageBackground>
 		);
   }
 }
 
 const styles = StyleSheet.create({
-  backgroundImage: {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-  },
-  container: {
-    height: "54%",
-    width: "88%",
-    backgroundColor: "#fff",
-    position: "absolute",
-    marginLeft: 10,
-    marginRight: 10,
-    bottom: 30,
-    borderRadius: 8,
-  },
-  title: {
-    fontSize: 45,
-    fontWeight: "600",
-    color: "#ffffff",
-    marginTop: 75,
-  },
-  textInput: {
-    fontSize: 16,
-    fontWeight: "300",
-    height: 50,
-    borderColor: "gray",
-    borderWidth: 1,
-    backgroundColor: "rgba(255, 255, 255, 0.9)",
-    color: "black",
-    marginTop: 15,
-    marginLeft: 20,
-    marginRight: 20,
-    padding: 10,
-    borderRadius: 8,
-  },
-  button: {
-    // flexDirection: "row",
-    backgroundColor: "#757083",
-    fontSize: 16,
-    // alignItems: "center",
-    // justifyContent: "center",
-    // margin: 12,
-    // elevation: 3,
-    // borderRadius: 12,
-    width: "80%",
-    marginBottom: 60,
-    // marginLeft: 30,
-    // marginRight: 30,
-    // opacity: 0.7,
-  },
-  box: {
-    flex: 1,
-    justifyContent: "space-around",
-    fontSize: 16,
-    fontWeight: "300",
-    color: "#757083",
-    marginBottom: -400,
-  },
-  colorBackground: {
-    marginTop: 10,
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    marginBottom: 20,
-  },
-  colorIcon: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    marginLeft: 10,
-    marginRight: 10,
-  },
-  option1: {
-    backgroundColor: "#090C08",
-  },
-  option2: {
-    backgroundColor: "#474056",
-  },
-  option3: {
-    backgroundColor: "#8A95A5",
-  },
-  option4: {
-    backgroundColor: "#B9C6AE",
-  },
+	backgroundImage: {
+		flex: 1,
+		width: '100%',
+		height: '100%',
+		alignItems: 'center',
+	},
+	container: {
+		height: '54%',
+		width: '88%',
+		backgroundColor: '#fff',
+		position: 'absolute',
+		marginLeft: 10,
+		marginRight: 10,
+		bottom: 30,
+		borderRadius: 8,
+	},
+	title: {
+		fontSize: 45,
+		fontWeight: '600',
+		color: '#ffffff',
+		marginTop: 75,
+	},
+	textInput: {
+		fontSize: 16,
+		fontWeight: '300',
+		width: '88%',
+		height: '50%',
+		borderColor: 'gray',
+		borderWidth: 1,
+		backgroundColor: 'rgba(255, 255, 255, 0.9)',
+		color: 'black',
+		marginTop: 15,
+		marginLeft: 20,
+		marginRight: 20,
+		padding: 10,
+		borderRadius: 8,
+	},
+	pickedColor: {
+		marginTop: 10,
+		flexDirection: 'row',
+		justifyContent: 'space-evenly',
+	},
+	chatButton: {
+		height: '50%',
+		backgroundColor: '#757083',
+		width: '88%',
+		alignItems: 'center',
+		// borderRadius: 12,
+	},
+	buttonText: {
+		fontSize: 16,
+		color: '#FFFFFF',
+		paddingTop: 17,
+	},
+	colorOption: {
+		width: 50,
+		height: 50,
+		borderRadius: 25,
+	},
+	button1: {
+		backgroundColor: '#090C08',
+	},
+	button2: {
+		backgroundColor: '#474056',
+	},
+	button3: {
+		backgroundColor: '#8A95A5',
+	},
+	button4: {
+		backgroundColor: '#B9C6AE',
+	},
+	box: {
+		flex: 1,
+		justifyContent: 'space-evenly',
+	},
+	alignment: {
+		alignSelf: 'center',
+	},
 });
