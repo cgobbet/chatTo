02789aa4ae7a4f5ce5f5188728ecffcb55869e4b
cgobbet <cassiano.gobbet@gmail.com>
2020-05-15 12:10:29 +0200

    Update App.js
    Remove ol comments

--- App.js
+++ App.js
@@ -3,21 +3,10 @@ import 'react-native-screens';
 import { StyleSheet, Text, View } from "react-native";
 
 import Chat from "./components/Chat";
-import CustomActions from './CustomActions';
 import React from "react";
-// import the screens
-// import Screen1 from "./components/Screen1";
-// import Screen2 from "./components/Screen2";
 import Start from "./components/Start";
-// import react Navigation
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-
-// Create the navigator
-// const navigator = createStackNavigator({
-//   Screen1: { screen: Screen1 },
-//   Screen2: { screen: Screen2 },
-// });
 
 const navigator = createStackNavigator({
   Start: { screen: Start },
