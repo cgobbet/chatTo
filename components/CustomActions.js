5ed0f5c7f10728ab48eafbaca54397946dbe907b
cgobbet <cassiano.gobbet@gmail.com>
2020-05-15 12:11:09 +0200

    Update CustomActions.js

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,4 +1,4 @@
-// import * as ImagePicker from 'expo-image-picker';
+import * as ImagePicker from 'expo-image-picker';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 
@@ -14,50 +14,62 @@ export default class CustomActions extends React.Component {
 		super()
 	}
 
-	pickImage = async () => {
-		const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+// selects images after permission granted	
+	async pickImage() {
+		try {
+			const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
 
-		if (status === 'granted') {
-			let result = await ImagePicker.launchImageLibraryAsync({
-				mediaTypes: 'Images',
-			}).catch(error => console.log(error));
+			if (status === 'granted') {
+				let result = await ImagePicker.launchImageLibraryAsync({
+					mediaTypes: ImagePicker.MediaTypeOptions.Images,
+					allowsEditing: true,
+					quality: 1,
+				}).catch(error => console.log(error));
 
-			if (!result.cancelled) {
-				this.setState({
-					image: result,
-				});
+				if (!result.cancelled) {
+					const imageUrl = await this.uploadImage(result.uri);
+					this.props.onSend({ image: imageUrl });
+				}
+				console.log(result);
 			}
+		} catch (error) {
+			console.log(error.message);
 		}
-	};
+	}
 
-	takePhoto = async () => {
-		const { status } = await Permissions.askAsync(Permissions.CAMERA);
+	// Take photo with device camera if permission granted
+	async takePhoto() {
+		try {
+			const { status } = await Permissions.askAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL);
 
-		if (status === 'granted') {
-			let result = await ImagePicker.launchCameraAsync({
-				mediaTypes: 'Images',
-			}).catch(error => console.log(error));
+			if (status === 'granted') {
+				let result = await ImagePicker.launchCameraAsync({
+					mediaTypes: ImagePicker.MediaTypeOptions.Images,
+				}).catch(error => console.log(error));
 
-			if (!result.cancelled) {
-				this.setState({
-					image: result,
-				});
+				if (!result.cancelled) {
+					const imageUrlLink = await this.uploadImage(result.uri);
+					this.props.onSend({ image: imageUrlLink });
+				}
 			}
+		} catch (error) {
+			console.log(error.message);
 		}
-	};
+	}
 
 	async getLocation() {
 		try {
 			const { status } = await Permissions.askAsync(Permissions.LOCATION);
 			if (status === 'granted') {
-				const location = await Location.getCurrentPositionAsync({})
-				.catch(error => console.log(error));
+				const location = await Location.getCurrentPositionAsync({}).catch(error =>
+					console.log(error),
+				);
 
 				if (location) {
 					this.props.onSend({
 						location: {
 							longitude: location.coords.longitude,
-							latitude: location.coords.latitude
+							latitude: location.coords.latitude,
 						}
 					});
 				}
@@ -68,23 +80,24 @@ export default class CustomActions extends React.Component {
   };
 
 	onActionPress = () => {
-		const options = ['Choose From Library', 'Take Picture', 'Send Location', 'Cancel'];
+		const options = ['Select Image from Library', 'Take a Photo', 'Share Location', 'Cancel'];
 		const cancelButtonIndex = options.length - 1;
 		this.context.actionSheet().showActionSheetWithOptions(
 			{
 				options,
 				cancelButtonIndex,
 			},
-			async (buttonIndex) => {
+			async buttonIndex => {
 				switch (buttonIndex) {
 					case 0:
-						console.log('user wants to pick an image');
-						return;
+						//console.log('pick image from library');
+						return this.pickImage();
 					case 1:
-						console.log('user wants to take a photo');
-						return;
+						//console.log('take photo');
+						return this.takePhoto();
 					case 2:
-						console.log('user wants to get their location');
+						//console.log('get location');
+						return this.getLocation();
 					default:
 				}
 			},
@@ -93,16 +106,15 @@ export default class CustomActions extends React.Component {
 
 	render() {
 		return (
-			<TouchableOpacity 
-				accessibility={true}
-				accessibilityLabel={'Click for options'}
-				style={[styles.container]} 
-				onPress={this.onActionsPress}>
-				
+			<TouchableOpacity
+				accessible={true}
+				accessibilityLabel='Click to see options!'
+				accessibilityHint='Action options allow you to pick from you library image, take a picture or share your location'
+				style={[styles.container]}
+				onPress={this.onActionPress}
+			>
 				<View style={[styles.wrapper, this.props.wrapperStyle]}>
-					<Text style={[styles.iconText, this.props.iconTextStyle]}>
-					+
-					</Text>
+					<Text style={[styles.iconText, this.props.iconTextStyle]}>+</Text>
 				</View>
 			</TouchableOpacity>
 		);
