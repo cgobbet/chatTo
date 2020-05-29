04a8d6f1f7f9291512ec24a5d902571e40d9b821
cgobbet <cassiano.gobbet@gmail.com>
2020-05-29 10:45:36 +0200

    Update App.js
    Eslint fixes

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
+// eslint-disable-next-line import/no-unresolved
 import 'react-native-screens';
 
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import Chat from './components/Chat';
+import Start from './components/Start';
+
+
 // chat screens
-import Chat from "./components/Chat";
-import Start from "./components/Start";
-//navigation
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+
+// navigation
 
 const navigator = createStackNavigator({
-	Start: {
-		screen: Start,
-		navigationOptions: {
-			//hide navigation bar
-			headerShown: false,
-		},
-	},
-	Chat: { screen: Chat },
+  Start: {
+    screen: Start,
+    navigationOptions: {
+      // hide navigation bar
+      headerShown: false,
+    },
+  },
+  Chat: { screen: Chat },
 });
 
 const navigatorContainer = createAppContainer(navigator);
